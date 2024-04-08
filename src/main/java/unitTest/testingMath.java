76004fb02831f3ec09cedab2d04f1b44a45ba04e
Eduart2021 <zeedymarina@yahoo.com>
2024-04-07 22:48:01 -0500

    change version

--- src/main/java/unitTest/testingMath.java
+++ src/main/java/unitTest/testingMath.java
@@ -7,12 +7,13 @@ public class testingMath  {
 
     mathClass mc = new mathClass();
     @Test
-    public void testAddition(){
+    public void testAddition(){  //positive test
         int result = mc.adding(2,2);
         Assert.assertEquals(result,4);
     }
     @Test
-    public void notAdding(){
+    public void notAdding(){  // negative test
+        //using a sting as an input
         int result = mc.adding(Integer.parseInt("7"),2);
         Assert.assertNotEquals(result,4);
     }
