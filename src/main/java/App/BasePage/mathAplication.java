94738f81c51f79b844a82e0f739e8702472f96e8
Eduart2021 <zeedymarina@yahoo.com>
2024-04-10 18:43:42 -0500

    add classes

---
+++ src/main/java/App/BasePage/mathAplication.java
@@ -0,0 +1,23 @@
+package App.BasePage;
+
+import App.Modulus.modularTheory;
+
+public class mathAplication {
+
+    public int adding(int numb1, int numb2){
+        return numb1 + numb2;
+    }
+    public int sub(int numb1, int numb2){
+        return numb1 - numb2;
+    }
+    public int multy(int numb1, int numb2){
+        return numb1 * numb2;
+    }
+    public int devis(int numb1, int numb2){
+        return numb1 / numb2;
+    }
+
+    public static String nameUser(String name){
+        return name;
+    }
+}
---
+++ src/main/java/App/Modulus/modularTheory.java
@@ -0,0 +1,11 @@
+package App.Modulus;
+
+public class modularTheory{
+
+    private String name;
+
+    public String appName(String name){
+        this.name = name;
+        return  this.name;
+    }
+}
